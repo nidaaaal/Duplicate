a1987a9b9b7c74b0fe72e976eb1111f2753229ad
nidal <nidalcheruvadi@gmail.com>
2025-05-26 17:10:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Duplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 11, 2, 3, 4, 4 };
+            HashSet<int> set = new HashSet<int>(arr);
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+                
+            }
+        }
+    }
+}
+
+    
